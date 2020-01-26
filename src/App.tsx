4335f21e8a72507c7228e05200433e21ae15a020
jovica-conkic <jovica.conkic@gmail.com>
2020-01-26 19:53:22 +0100

    Fixing the heading style

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ class App extends Component<IProps, IState> {
     );
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1 className="app-heading">Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
